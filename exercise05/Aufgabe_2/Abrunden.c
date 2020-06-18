7db80c0cf6cbce1fc566342149c210a537b6e95c
eppdo <67054211+eppdo@users.noreply.github.com>
2020-06-18 17:05:00 +0200

    Update Abrunden.c

--- exercise05/Aufgabe_2/Abrunden.c
+++ exercise05/Aufgabe_2/Abrunden.c
@@ -6,7 +6,7 @@ float Abrunden(float in, int Rundungsstelle)
 	float out;
 
 	out = floorf(in * pow(10, Rundungsstelle)) / pow(10, Rundungsstelle);
-	printf("Gerundet: %.2f\n", out);
+	//printf("Gerundet: %.2f\n", out);
 
 
 	return out;
